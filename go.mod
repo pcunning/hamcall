d14dd73f2e640d3a56ad95957795eddfdde75aae
Pat Cunningham <pat@c2.com>
2021-06-19 20:38:32 -0700

    inital

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/pcunning/hamcall
+
+go 1.15
+
+require (
+	github.com/imdario/mergo v0.3.12 // indirect
+	github.com/jlaffaye/ftp v0.0.0-20210307004419-5d4190119067 // indirect
+)
---
+++ main.go
@@ -0,0 +1,321 @@
+package main
+
+import (
+	"archive/zip"
+	"encoding/csv"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"path/filepath"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/imdario/mergo"
+	"github.com/jlaffaye/ftp"
+)
+
+type HamCall struct {
+	Callsign string `json:"callsign"`
+	Name     string `json:"name"`
+	Class    string `json:"class"`
+	Address  string `json:"address"`
+	City     string `json:"city"`
+	State    string `json:"state"`
+	Zip      string `json:"zip"`
+	LOTW     string `json:"last_lotw"`
+}
+
+func main() {
+
+	// var wg sync.WaitGroup
+
+	// wg.Add(1)
+	// go DownloadFile("lotw.csv", "https://lotw.arrl.org/lotw-user-activity.csv", &wg)
+
+	// wg.Add(1)
+	// go DownloadFTPFile("amat.zip", "ftp://wirelessftp.fcc.gov:21/pub/uls/complete/l_amat.zip", &wg)
+
+	// wg.Wait()
+
+	// files, err := Unzip("amat.zip", "amat")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Println("Unzipped:\n" + strings.Join(files, "\n"))
+
+	// ProcessAM()
+	// ProcessEN()
+	ProcessLOTW()
+
+}
+
+// DownloadFile will download a url to a local file. It's efficient because it will
+// write as it downloads and not load the whole file into memory.
+// From https://golangcode.com/download-a-file-from-a-url/
+func DownloadFile(filepath string, u string, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	// Get the data
+	resp, err := http.Get(u)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	// Create the file
+	out, err := os.Create(filepath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer out.Close()
+
+	// Write the body to file
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func DownloadFTPFile(filepath string, u string, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	pu, _ := url.Parse(u)
+
+	c, err := ftp.Dial(pu.Host, ftp.DialWithTimeout(5120*time.Second))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = c.Login("anonymous", "anonymous")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	resp, err := c.Retr(pu.Path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create the file
+	out, err := os.Create(filepath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer out.Close()
+
+	// Write the body to file
+	_, err = io.Copy(out, resp)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := c.Quit(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+// Unzip will decompress a zip archive, moving all files and folders
+// within the zip file (parameter 1) to an output directory (parameter 2).
+// https://golangcode.com/unzip-files-in-go/
+func Unzip(src string, dest string) ([]string, error) {
+
+	var filenames []string
+
+	r, err := zip.OpenReader(src)
+	if err != nil {
+		return filenames, err
+	}
+	defer r.Close()
+
+	for _, f := range r.File {
+
+		// Store filename/path for returning and using later on
+		fpath := filepath.Join(dest, f.Name)
+
+		// Check for ZipSlip. More Info: http://bit.ly/2MsjAWE
+		if !strings.HasPrefix(fpath, filepath.Clean(dest)+string(os.PathSeparator)) {
+			return filenames, fmt.Errorf("%s: illegal file path", fpath)
+		}
+
+		filenames = append(filenames, fpath)
+
+		if f.FileInfo().IsDir() {
+			// Make Folder
+			os.MkdirAll(fpath, os.ModePerm)
+			continue
+		}
+
+		// Make File
+		if err = os.MkdirAll(filepath.Dir(fpath), os.ModePerm); err != nil {
+			return filenames, err
+		}
+
+		outFile, err := os.OpenFile(fpath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+		if err != nil {
+			return filenames, err
+		}
+
+		rc, err := f.Open()
+		if err != nil {
+			return filenames, err
+		}
+
+		_, err = io.Copy(outFile, rc)
+
+		// Close the file without defer to close before next iteration of loop
+		outFile.Close()
+		rc.Close()
+
+		if err != nil {
+			return filenames, err
+		}
+	}
+	return filenames, nil
+}
+
+func ProcessAM() {
+	fmt.Println("processing AM")
+
+	f, err := os.Open("amat/AM.dat")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer f.Close()
+
+	r := csv.NewReader(f)
+	r.Comma = '|'
+	r.LazyQuotes = true
+	r.FieldsPerRecord = -1
+
+	i := 1
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			break
+		}
+
+		OpenUpdateCall(&HamCall{
+			Callsign: record[4],
+			Class:    record[5],
+		})
+
+		if i%1000 == 1 {
+			fmt.Printf(".")
+		}
+		i++
+	}
+}
+
+func ProcessEN() {
+
+	fmt.Println("processing EN")
+
+	f, err := os.Open("amat/EN.dat")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer f.Close()
+
+	r := csv.NewReader(f)
+	r.Comma = '|'
+	r.LazyQuotes = true
+	r.FieldsPerRecord = -1
+
+	i := 1
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			break
+		}
+
+		OpenUpdateCall(&HamCall{
+			Callsign: record[4],
+			Name:     record[7],
+			Address:  record[15],
+			City:     record[16],
+			State:    record[17],
+			Zip:      record[18],
+		})
+
+		if i%1000 == 1 {
+			fmt.Printf(".")
+		}
+		i++
+	}
+
+}
+
+func ProcessLOTW() {
+
+	fmt.Println("processing EN")
+
+	f, err := os.Open("lotw.csv")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer f.Close()
+
+	r := csv.NewReader(f)
+	r.FieldsPerRecord = -1
+
+	i := 1
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			break
+		}
+
+		OpenUpdateCall(&HamCall{
+			Callsign: record[0],
+			LOTW:     record[1] + record[2],
+		})
+
+		if i%1000 == 1 {
+			fmt.Printf(".")
+		}
+		i++
+	}
+}
+
+func OpenUpdateCall(data *HamCall) {
+	var hc HamCall
+
+	filename := "calls/" + data.Callsign + ".json"
+	jsonFile, err := os.Open(filename)
+	if err != nil {
+		// fmt.Println(err)
+	} else {
+		defer jsonFile.Close()
+		byteValue, _ := ioutil.ReadAll(jsonFile)
+		json.Unmarshal(byteValue, &hc)
+	}
+
+	if err := mergo.Merge(&hc, data); err != nil {
+		fmt.Println(err)
+	}
+
+	file, _ := json.Marshal(hc)
+	err = ioutil.WriteFile(filename, file, 0644)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+}
