14aea948b4df57b8197d4f44442ce98444611728
Patrick Cunningham <pcunning@users.noreply.github.com>
2024-07-13 13:59:58 -0700

    LOTW back

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ import (
 	"github.com/pcunning/hamcall/b2"
 	"github.com/pcunning/hamcall/data"
 	"github.com/pcunning/hamcall/source/geo"
-	// "github.com/pcunning/hamcall/source/lotw"
+	"github.com/pcunning/hamcall/source/lotw"
 	"github.com/pcunning/hamcall/source/radioid"
 	"github.com/pcunning/hamcall/source/uls"
 
@@ -86,11 +86,11 @@ func main() {
 func downloadFiles() {
 	var wg sync.WaitGroup
 
-	wg.Add(3)
+	wg.Add(4)
 
 	go uls.Download(&wg)
 	go radioid.Download(&wg)
-	// go lotw.Download(&wg)
+	go lotw.Download(&wg)
 	go geo.Download(&wg)
 
 	wg.Wait()
@@ -99,7 +99,7 @@ func downloadFiles() {
 func process(calls *map[string]data.HamCall) {
 	uls.Process(calls)
 	radioid.Process(calls)
-	// lotw.Process(calls)
+	lotw.Process(calls)
 	geo.Process(calls)
 }
 
