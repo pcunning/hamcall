eb8741274e01c49d739348aef372d86a8dd8a7ce
Patrick Cunningham <pcunning@users.noreply.github.com>
2025-03-12 13:40:50 -0700

    Update data.go

--- data/data.go
+++ data/data.go
@@ -4,21 +4,21 @@ type HamCall struct {
 	Callsign   string    `json:"callsign"`
 	Name       string    `json:"name"`
 	FirstName  string    `json:"first_name"`
-	Mi         string    `json:"mi"`
+	Mi         string    `json:"mi,omitempty"`
 	LastName   string    `json:"last_name"`
 	Class      string    `json:"class"`
-	Address    string    `json:"address"`
-	City       string    `json:"city"`
-	State      string    `json:"state"`
-	Zip        string    `json:"zip"`
-	PoBox      string    `json:"po_box"`
-	Grant      string    `json:"grant"`
-	Effective  string    `json:"effective"`
-	Expiration string    `json:"expiration"`
-	FRN        string    `json:"frn"`
-	FileNumber string    `json:"file_number"`
-	LOTW       string    `json:"last_lotw"`
-	LicenseKey string    `json:"license_key"`
+	Address    string    `json:"address,omitempty"`
+	City       string    `json:"city,omitempty"`
+	State      string    `json:"state,omitempty"`
+	Zip        string    `json:"zip,omitempty"`
+	PoBox      string    `json:"po_box,omitempty"`
+	Grant      string    `json:"grant,omitempty"`
+	Effective  string    `json:"effective,omitempty"`
+	Expiration string    `json:"expiration,omitempty"`
+	FRN        string    `json:"frn,omitempty"`
+	FileNumber string    `json:"file_number,omitempty"`
+	LOTW       string    `json:"last_lotw,omitempty"`
+	LicenseKey string    `json:"license_key,omitempty"`
 	DMRID      []int     `json:"dmr_id,omitempty"`
 	Location   *Location `json:"location,omitempty"`
 }
